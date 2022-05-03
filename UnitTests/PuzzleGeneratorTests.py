69473ead4f88aee8e124d08779d5c0b371214eb6
Thomas <thomasknechtel@outlook.com>
2022-05-02 21:02:42 -0400

    added unit test for updateBoard

--- UnitTests/PuzzleGeneratorTests.py
+++ UnitTests/PuzzleGeneratorTests.py
@@ -10,7 +10,24 @@ from GameAnalysis import GameAnalysis
 class TestAnalysis(unittest.TestCase):
     def setUp(self) -> None:
         self.gameAnalysis = GameAnalysis(None, 16, 4)
+    def test_updateBoard(self):
+        """test if AnalysisGame.updateBoard updates the board fen correctly and returns true if valid move"""
+        fenStart = "r1bqkb1r/pppp1ppp/2n2n2/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4"
+        self.gameAnalysis.board = chess.Board(fenStart)
+        self.assertTrue(self.gameAnalysis.updateBoard(chess.Move.from_uci('b5c6')))
+        self.assertEqual("r1bqkb1r/pppp1ppp/2B2n2/4p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 4", self.gameAnalysis.board.fen())
 
+        fenStart = "r1bqkb1r/ppp2ppp/2p2n2/4p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 5"
+        self.gameAnalysis.board = chess.Board(fenStart)
+        self.assertTrue(self.gameAnalysis.updateBoard(chess.Move.from_uci('e1g1')))
+        self.assertEqual("r1bqkb1r/ppp2ppp/2p2n2/4p3/4P3/5N2/PPPP1PPP/RNBQ1RK1 b kq - 1 5", self.gameAnalysis.board.fen())
+
+        fenStart = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        self.gameAnalysis.board = chess.Board(fenStart)
+        self.assertFalse(self.gameAnalysis.updateBoard(chess.Move.from_uci('c1g2')))
+        self.assertEqual(fenStart, self.gameAnalysis.board.fen())
+        
+        
     def test_isWinning(self)->None:
         """test if function isWinning returns true if a player is winning.
         Cases: Relative CentiPawn Score = 230, turn = WHITE return True
