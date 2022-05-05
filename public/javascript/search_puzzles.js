c8d19454e9d62eb4688768c23407ff28ceb41ff4
Thomas <thomasknechtel@outlook.com>
2022-05-05 14:51:41 -0400

    fix display board

--- public/javascript/search_puzzles.js
+++ public/javascript/search_puzzles.js
@@ -31,19 +31,17 @@ const rowClicked = function rowClicked(elem){
  * @param {HTMLElement} elem 
  */
 const hover = function hover(elem){
-    xpos = window.event.x + document.body.scrollLeft - 2;
-    ypos = window.event.y + document.body.scrollTop - 2;
     const fen = this.innerHTML
     const boardDiv = document.createElement('div')
     boardDiv.id='myBoard'
     boardDiv.classList.add('board')
     boardDiv.style.position='absolute'
-    boardDiv.style.left=xpos+'px'
-    boardDiv.style.top=ypos-200+'px'
+    boardDiv.style.right=0
+    boardDiv.style.top=-200+"px"
     boardDiv.style.width='200px'
     
     boardDiv.style.zIndex='1'
-    document.getElementsByTagName("body")[0].innerHTML+=boardDiv.outerHTML
+    this.innerHTML+=boardDiv.outerHTML
    
     
     const board = new ChessBoard('myBoard', fen)
